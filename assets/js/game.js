dc5d6505e52829d6df7a35d400bd4d931e046060
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2021-11-20 23:03:22 -0600

    random order feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -38,8 +38,14 @@ return false;
 
 var fight = function(enemy) {
   // repeat and execute as long as the enemy-robot is alive
+  // keep track of who goes first
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
 
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
     // ask player if they'd like to fight or skip using fightOrSkip function
     if (fightOrSkip()) {
       // if true, leave fight by breaking loop
@@ -64,6 +70,8 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
+    // player gets attacked first
+  } else {
 
     // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
@@ -83,6 +91,9 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
+ }
 };
 
 // function to start a new game
